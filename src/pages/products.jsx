169202eecd5d2408980ef77545aef281b952367f
Komiljon <nortoshovk@gmail.com>
2024-09-05 10:32:16 +0500

    Added products

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,8 +1,43 @@
+import { useCallback, useEffect, useState } from "react";
+import Loader from "../components/loader";
+import Card from "../components/card";
+
 function Products() {
+  const [data, setData] = useState(null);
+  const [loader, setLoader] = useState(true);
+
+  const getData = useCallback(async () => {
+    try {
+      setLoader(true);
+      const response = await fetch("https://fakestoreapi.com/products");
+
+      if (!response.ok) {
+        throw new Error(`Could not fetch ${url}, status: ${response.status}`);
+      }
+
+      const result = await response.json();
+      setData(result);
+      setLoader(false);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    getData();
+  }, [getData]);
   return (
-    <div>
-      <h1>Products</h1>
-    </div>
+    <section className="products">
+      {data ? (
+        <div className="products__cards">
+          {data.map((product) => (
+            <Card key={product.id} {...product} />
+          ))}
+        </div>
+      ) : (
+        <Loader />
+      )}
+    </section>
   );
 }
 
